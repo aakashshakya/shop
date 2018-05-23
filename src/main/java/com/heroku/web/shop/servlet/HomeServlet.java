5ea478d2babab5550bf2b0da74992e8652c0659e
Aakash Raj Shakya <aakash_shakya@hotmail.com>
2018-05-23 17:30:33 +0545

    First Commit

---
+++ src/main/java/com/heroku/web/shop/servlet/HomeServlet.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.heroku.web.shop.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author user
+ */
+@WebServlet(name = "home", urlPatterns = {"/","/home"})
+public class HomeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().println("<h1>Shop test has finally worked. Hurray<h1>");
+    }
+
+}
